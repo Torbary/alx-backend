66ef358a746f0cc056fc0c54f010ee8cde1f7851
Torbary <torbarey020@gmail.com>
2023-08-18 18:17:01 +0100

    created dir & moved the files into it

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module for pagination index range calculation.
+"""
+
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Calculates the start and end indices for pagination.
+
+    Args:
+        page (int): The current page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start and end indices.
+    """
+    # Calculate the start index based on the given page number and page size
+    start_index = (page - 1) * page_size
+
+    # Calculate the end index
+    end_index = start_index + page_size
+
+    return start_index, end_index - 1  # Ajusting the end index to be inclusive
